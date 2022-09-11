40845c89c23a1ed74d63fac4532a464abf21c5c2
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-09-11 11:40:19 +0200

    fix: added parenthesis

--- lib/election.ex
+++ lib/election.ex
@@ -2,8 +2,8 @@ defmodule Election do
   defstruct(
     name: "Mayor",
     candidates: [
-      Candidate.new(1, "Will Ferrell",
-      Candidate.new(2, "Kristen Wiig",
+      Candidate.new(1, "Will Ferrell"),
+      Candidate.new(2, "Kristen Wiig"),
     ],
     next_id: 3
   )
