37d2c1459a99f19de6d683a39e9306f3ee89047d
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-09-11 11:37:58 +0200

    initial commit

---
+++ lib/election.ex
@@ -0,0 +1,17 @@
+defmodule Election do
+  defstruct(
+    name: "Mayor",
+    candidates: [
+      Candidate.new(1, "Will Ferrell",
+      Candidate.new(2, "Kristen Wiig",
+    ],
+    next_id: 3
+  )
+
+  def view_header(election) do
+    [
+      "Election for #{election.name}\n",
+    ]
+  end
+end
+
