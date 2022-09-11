84704a374091092478bcc0a8db1d01501862b45c
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-09-11 14:19:13 +0200

    feat: update function for name

--- lib/election.ex
+++ lib/election.ex
@@ -8,6 +8,11 @@ defmodule Election do
     next_id: 3
   )
 
+  def update(election, ["n" <> _rest | args]) do
+    name = Enum.join(args, " ")
+    Map.put(election, :name, name)
+  end
+
   def view(election) do 
     [
       view_header(election),
